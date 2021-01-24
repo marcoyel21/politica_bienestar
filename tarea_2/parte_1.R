4211daa552096996169a27a5fe06708b5e4de736
marcoyel21 <marcoyel21@gmail.com>
2021-01-24 00:21:12 -0600

    r_script

--- tarea_2/parte_1.R
+++ tarea_2/parte_1.R
@@ -9,11 +9,30 @@ library(corrplot)
 data<-read_csv("pobreza_18.csv")
 #creo una variable del grupo 3 (mi objetivo)
 data<-data%>%mutate(y_3=ifelse(data$cuadrantes==3,1,0))
-#limpio la base de datos
 
-#Parte II
-# Realizo la correlación de todas las variables y simpelemente tomo las más grandes en valor absoluto.
+#Parte II 
 
+#Limpieza
+
+# De todas estas columnas con Nas solo puedo salvar sa_dir
+colSums(is.na(data))
+data$inas_esc<-NULL
+data$pea<-NULL
+data$pam<-NULL
+data$hli<-NULL
+data$tot_iaad<-NULL
+data$tot_iamen<-NULL
+data$niv_ed<-NULL
+data$ent<-NULL  #Entidad no muestra relación con nuestra variable objetivo
+summary(factor(data$sa_dir))
+data$sa_dir[is.na(data$sa_dir)] <- 0
+data<-na.omit(data)
+
+#Analisis de correlaciones (sin expansion)
+cor_data<-as.data.frame(cor(data))
+cor_data_filtered<-cor_data %>%select(y_3)
+#De aqui tomo las variables más correlacionadas positiva y negativamente
+# Y paso a la revision de literatura para evaluar cuales son las mejores variables
 
 # Parte III
 #Propongo el siguiente modelo
@@ -21,7 +40,6 @@ data_filtered<-data%>%select(y_3,factor,
                              plb, s_salud, jef_ss,ss_dir,
                               rururb,ic_sbv,tam_loc ,ic_rezedu, 
                                ic_asalud ,ic_cv,isb_combus,ic_ali)
-data_filtered<-na.omit(data_filtered)
 data_filtered<-expandRows(data_filtered,"factor")
 
 #Como la base de datos es muy grande, propongo una muestra aleatoria de tamaño n para "jugar"

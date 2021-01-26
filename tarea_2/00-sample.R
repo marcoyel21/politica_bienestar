ae27e3b3c18ccc6b7821a910607ef2501efac65a
marcoyel21 <marcoyel21@gmail.com>
2021-01-26 02:32:09 -0600

    update

--- tarea_2/00-sample.R
+++ tarea_2/00-sample.R
@@ -9,14 +9,14 @@ data<-data%>%mutate(y_3=ifelse(data$cuadrantes==3,1,0))
 
 #filtro de acuerdo a lo que necesito
 data$sa_dir<-NULL
-data<-data%>% select(factor,y_3,tam_loc,rururb,ic_rezedu,
+data<-data%>% select(factor,y_3,cuadrantes,tam_loc,rururb,ic_rezedu,
                      ic_asalud,ic_segsoc,jef_ss,
                      s_salud,isb_combus,ins_ali,ictpc,
-                     carencias,ic_cv,ic_ali,plb,ss_dir,ic_sbv)
+                     carencias,ic_cv,ic_ali,plb,ss_dir,ic_sbv,ing_mon)
 data<-na.omit(data)
 
 data<-expandRows(data,'factor')
 
 sample<-data[sample(nrow(data), 200000), ]
 
-
+write_csv(sample,"muestra_idd.csv")

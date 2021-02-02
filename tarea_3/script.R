cc89962cca16d66c434ee1b919f8e5ebd5261f84
marcoyel21 <marcoyel21@gmail.com>
2021-02-01 23:11:05 -0600

    update

--- tarea_3/script.R
+++ tarea_3/script.R
@@ -30,9 +30,68 @@ clean<-read.csv("clean.csv")
 data_2 <- left_join( wpt_2, clean,
                    by = c("countrycode"="countrycode", "year"="year"))  #Esta es la base de datos final
 
+data_2<- data_2%>% 
+  group_by(countrycode) %>%
+  mutate(growth = c(NA,diff(rgdpe))/lag(rgdpe, 1))
+
 #data_2 es la base final
 
 
 #Parte III: modelaje
 
+install.packages("plm")
+library(plm)
+
+# Modelo 2: Efectos fijos
+
+
+
+efectos_fijos_tiempo <- plm( growth  ~ gini, 
+                      data = data_2,
+                      index = c("countrycode", "year"), 
+                      model = "within",
+                      effect = "time")
+
+efectos_fijos_pais <- plm( growth  ~ gini, 
+                      data = data_2,
+                      index = c("countrycode", "year"), 
+                      model = "within",
+                      effect = "individual")
+
+efectos_fijos_ambos <- plm( growth  ~ gini, 
+                           data = data_2,
+                           index = c("countrycode", "year"), 
+                           model = "within",
+                           effect = "twoways")
+summary(efectos_fijos_tiempo)
+summary(efectos_fijos_pais)
+summary(efectos_fijos_ambos)
+
+
+# De entrada en el modelo más sencillo de efectos fijos ya sabemos que el gini está correlacionado de manera negativa con el crecimiento.
+# Aunque la r^2 es demasiado baja, el coeficiente es negativo y es estadisticamente significativo a más del 99%
+
+#En cuanto a efectos fijos, el modelo que contempla efectos fijos para cada país tuvo la R^2 más alta; por el contrario, el modelo que contemplo solo efecto fijo para el tiempo tuvo la más baja
+
+efectos_fijos_region <- plm( growth  ~ gini+region, 
+                            data = data_2,
+                            index = c("countrycode", "year"), 
+                            model = "within",
+                            effect = "individual")
+summary(efectos_fijos_region)
+
+#Es interesante que si agregamos la variable de region al modelo de efectos fijos que ya contemplaba al país individual, en realidad no cambia NADA del modelo.
+
+efectos_fijos_region_2 <- plm( growth  ~ gini+region, 
+                             data = data_2,
+                             index = c("countrycode", "year"), 
+                             model = "within",
+                             effect = "time")
+summary(efectos_fijos_region_2)
+
+# Finalmente, si solo dejamos un efecto fijo para el tiempo y usamos region en lugar de un efecto fijo para cada país, obtenemos un modelo con la R^2 más alta (aunque sigue siendo diminuta)
+
+
+
+# Modelo 3: Efectos aleatorios
 
